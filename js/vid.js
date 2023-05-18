792f82d2f94248589925377882cce28f3236f734
Dylan Standing <dylanjstanding@gmail.com>
2023-05-18 14:31:30 -0600

    created main landing, art, portfolio, main animation

---
+++ js/vid.js
@@ -0,0 +1,14 @@
+// const video = document.getElementById("myVideo");
+// const speakerIcon = document.getElementById("speakerIcon");
+
+//speakerIcon.addEventListener("click", toggleMute);
+
+ //function toggleMute() {
+ // if (video.muted) {
+ //   video.muted = false;
+ //   speakerIcon.style.backgroundImage = "url('speaker-icon.png')";
+ // } else {
+ //   video.muted = true;
+ //   speakerIcon.style.backgroundImage = "url('speaker-mute-icon.png')";
+ // }
+// } 
